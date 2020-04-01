c1d1339ec3a07c372517bf166d2c44653daf41a2
Vivek Verma <vivnps.verma@gmail.com>
2020-04-01 00:53:22 -0700

    Image working

--- env.py
+++ env.py
@@ -15,7 +15,7 @@ class CoronaEnv:
             0 -> stay home
             1 -> go outside
         """
-        cv2.imshow("Corona", self.im2 if action else self.im2)
+        cv2.imshow("Corona", self.im2 if action else self.im1)
         reward = -1 if action else 1
         done = not(bool(action))
         return action, reward, done
--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 from env import CoronaEnv
 import time
+import cv2
 
 env = CoronaEnv()
 
-#while True:
-#    env.step(1)
-#    time.sleep(1)
-#    env.step(0)
-#    time.sleep(1)
+while True:
+    env.step(1)
+    #time.sleep(1)
+    if cv2.waitKey(1000) & 0xFF == 27:
+        exit()
+    env.step(0)
+    #time.sleep(1)
+    if cv2.waitKey(1000) & 0xFF == 27:
+        exit()
 
