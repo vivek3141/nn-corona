d18c66f7cc840638c456b00c7e27396a6f9a9174
Vivek Verma <vivnps.verma@gmail.com>
2020-04-01 01:19:26 -0700

    test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from env import CoronaEnv
 import cv2
 import neat
+import pickle
 
 im1 = cv2.imread("home.png")
 im2 = cv2.imread("away.png")
@@ -10,15 +11,21 @@ reward = 0
 
 env = CoronaEnv()
 
+genome = pickle.load(open("winner.pkl", 'rb'))
+
 net = neat.nn.FeedForwardNetwork.create(genome, config)
 config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
                          config_file)
-genome = pickle.load(open("winner.pkl", 'rb'))
 
+
+i = 1
 
 while True:
     cv2.imshow("Corona", im2 if state else im1)
     state, delta, done = env.step(net.activate(state))
+    reward += delta
+    print(f"Step {i}: Reward {reward} Done: {done}")
+    i += 1 
     if cv2.waitKey(1000) & 0xFF == 27:
         exit()
